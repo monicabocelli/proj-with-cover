84c8f4af4870ff1e17c0a8a3eeb801d37f75b0b5
monicabocelli <monica.bocelli93@gmail.com>
2017-02-13 15:21:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,12 +6,11 @@ var maxEnergy= 1000; //max energy for eathquake
 var button1;
 var button2;
 
-var cover;
 var myImage1;
 var myImage2;
 
 function preload() {
-    cover = loadImage("images/Tavola disegno 18-50.jpg");
+   
     myImage1 = loadImage("images/prova1.png");
     myImage2 = loadImage("images/prova2.jpg");
 }
@@ -26,7 +25,7 @@ function draw(){
      var magnitude = int(map(energy, 0, 1000, 0, 10)); 
     
      if (energy < 0.5) {
-     backgroundImage(cover,0,0,width,height);
+
      textSize(height/15);
      textAlign(CENTER);
      textStyle(BOLD);
